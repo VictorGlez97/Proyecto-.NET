96298215d3de32034e31068efb51e8cf98820234
VictorGlez97 <vichgleza@gmail.com>
2019-10-07 22:28:56 -0500

    .l.

--- Aplicacion-1/App_Code/Correo.cs
+++ Aplicacion-1/App_Code/Correo.cs
@@ -1,22 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Mail;
 
 public class Correo
 {
-    /*private MailMessage CorreoElectronico;
+    private MailMessage CorreoElectronico;
     private SmtpClient EnvioCorreo;
     private String MailFrom;
     private String Password;
-    public Attachment Anexos;*/
+    public Attachment Anexos;
 
     public Correo()
     {
-        /*CorreoElectronico = new MailMessage("miguel_vara@hotmail.com", "migueldelavararamirez@gmail.com");
+        CorreoElectronico = new MailMessage("vichgleza@hotmail.com", "vichgleza@gmail.com");
         EnvioCorreo       = new SmtpClient("smtp.live.com", 587);
-        MailFrom          = "miguel_vara@hotmail.com";
-        Password          = "********";   */ 
+        MailFrom          = "victor19031997@hotmail.com";
+        Password          = "";   
     }
 
+    public bool EnviarMail(string Mensaje)
+    {
+        try
+        {
+            CorreoElectronico.Body = Mensaje;
+            CorreoElectronico.Subject = "Pregunta Por Carreras Disponibles";
+
+            EnvioCorreo.Credentials = new NetworkCredential(MailFrom, Password);
+            EnvioCorreo.EnableSsl = true;
+            EnvioCorreo.DeliveryMethod = SmtpDeliveryMethod.Network;
+            CorreoElectronico.Body = Mensaje;
+            CorreoElectronico.Attachments.Add(Anexos);
+
+            EnvioCorreo.Send(CorreoElectronico);
+
+            return true;
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.StackTrace);
+            return false;
+        }
+    }
 }
--- Aplicacion-1/NET-Equipo7-Unidad0102.aspx.cs
+++ Aplicacion-1/NET-Equipo7-Unidad0102.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.IO;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net.Mail;
 using System.Text.RegularExpressions;
 
 namespace Aplicacion_1
@@ -17,6 +20,8 @@ namespace Aplicacion_1
 
         protected void btn_enviar_Click(object sender, EventArgs e)
         {
+            //Correo Mail = new Correo();
+            Correo c = new Correo();
 
             if (!Regex.IsMatch(txt_correo.Text, @"") || txt_correo.Text == "")
             {
@@ -42,6 +47,44 @@ namespace Aplicacion_1
                 Response.Redirect("/NET-Equipo7-Unidad0102.aspx");
             }
 
+            if (archivo.HasFile)
+            {
+                foreach (HttpPostedFile file in archivo.PostedFiles)
+                {
+                    string fileName = Path.GetFileName(file.FileName);
+
+                    string strExtension = Path.GetExtension(fileName);
+                    if (strExtension == ".png" || strExtension == ".jpg" || strExtension == ".jpeg")
+                    {
+                        file.SaveAs(Server.MapPath("~/") + fileName);
+                        c.Anexos = new Attachment(file.InputStream, fileName);
+                    }
+                }
+            }
+
+            StringBuilder BodyMesage = new StringBuilder();
+
+            BodyMesage.AppendLine("Solicitante: " + txt_nombre.Text);
+            BodyMesage.AppendLine("Nacimiento: " + nacimiento);
+            BodyMesage.AppendLine("Correo: " + txt_correo.Text);
+            BodyMesage.AppendLine("Telefono: " + txt_tel.Text);
+            BodyMesage.AppendLine("CURP: " + txt_CURP.Text);
+            BodyMesage.AppendLine("RFC: " + txt_RFC.Text);
+
+            if (c.EnviarMail(BodyMesage.ToString()) == true)
+            {
+                Response.Write("<script> alert('Solicitud entregada con EXITO'); </script>");
+                Response.Redirect("/");
+            }
+            else
+            {
+                Response.Write("<script> alert('ERROR: no se ha podido enviar la solicitud'); </script>");
+            }
+
         }
+    }
+
+    internal class Correo
+    {
     }
 }
